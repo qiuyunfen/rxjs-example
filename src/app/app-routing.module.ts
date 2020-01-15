73c67e77aa16064f8a666c7e1ba31d92a47d1bc7
qiuyunfen <yfqiu@thoughtworks.com>
2020-01-15 16:20:15 +0800

    observable switchMap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ObservableCreateComponent} from './observable-create/observable-create.component';
+import {ObservableOperatorComponent} from './observable-operator/observable-operator.component';
 
 const routes: Routes = [
   { path: 'observable-create', component: ObservableCreateComponent },
+  { path: 'observable-operator', component: ObservableOperatorComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ObservableCreateComponent } from './observable-create/observable-create.component';
+import { ObservableOperatorComponent } from './observable-operator/observable-operator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ObservableCreateComponent,
+    ObservableOperatorComponent,
   ],
   imports: [
     BrowserModule,
