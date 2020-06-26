ef07de2d4e00bf2b3ee94e81dcdb6ed18da1fda7
qiuyunfen <yfqiu@thoughtworks.com>
2020-06-26 14:41:38 +0800

    complete timeline

--- src/app/observable-monitor/observable-monitor.component.ts
+++ src/app/observable-monitor/observable-monitor.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {TimeService} from '../time.service';
 
 @Component({
@@ -11,12 +11,24 @@ export class ObservableMonitorComponent implements OnInit {
   @Input()
   name = '';
 
+  globalID;
+
   constructor(private timeService: TimeService) {
   }
 
+  repeatOften() {
+    const width = document.getElementById('timeline').style.width.slice(0, -2) || '0';
+    document.getElementById('timeline').style.width = parseInt(width) + 1 + 'px';
+    this.globalID = requestAnimationFrame(this.repeatOften.bind(this));
+  }
+
   ngOnInit() {
-    this.timeService.currentStatus.subscribe(message => {
-      console.log('------------', message);
+    this.timeService.currentStatus.subscribe(status => {
+      if (status) {
+        this.globalID = requestAnimationFrame(this.repeatOften.bind(this));
+      } else if (this.globalID) {
+        cancelAnimationFrame(this.globalID);
+      }
     });
   }
 
