5a7e3adfd86e325bbfeb2fd3d31d30c1b107916f
qiuyunfen <yfqiu@thoughtworks.com>
2020-06-28 19:05:58 +0800

    high observable modify

--- src/app/observable-monitor/observable-monitor.component.ts
+++ src/app/observable-monitor/observable-monitor.component.ts
@@ -1,10 +1,11 @@
 import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {TimeService} from '../time.service';
-import {BehaviorSubject, Observable} from 'rxjs';
+import {BehaviorSubject, Observable, Subscription} from 'rxjs';
 
 const MONITOR = {
   VALUE: 'value',
   SUBSCRIBE: 'subscribe',
+  INNER: 'inner',
 };
 
 
@@ -17,6 +18,8 @@ export class ObservableMonitorComponent implements OnInit {
 
   // @ts-ignore
   @ViewChild('timeline') timeline: ElementRef;
+  // @ts-ignore
+  @ViewChild('container') container: ElementRef;
   @Input()
   name = '';
 
@@ -29,19 +32,33 @@ export class ObservableMonitorComponent implements OnInit {
   @Input()
   isSubject = false;
 
+  @Input()
+  isHigh = false;
+
   subject$ = new BehaviorSubject(null);
   subject = this.subject$ as Observable<any>;
   globalID;
   subscription;
   current = 0;
+  animationIds: number[] = [];
+  subs = [];
 
   constructor(private timeService: TimeService) {
   }
 
   observe() {
     this.subject$.next({ type: MONITOR.SUBSCRIBE });
+    let count = 0;
     this.observable.subscribe((value) => {
-      this.subject$.next({ value: JSON.stringify(value), type: MONITOR.VALUE });
+      if (this.isHigh) {
+        this.subject$.next({
+          value,
+          type: MONITOR.INNER,
+          level: count++
+        });
+      } else {
+        this.subject$.next({ value: JSON.stringify(value), type: MONITOR.VALUE });
+      }
     }, null, () => {
       this.subject$.complete();
     });
@@ -55,20 +72,26 @@ export class ObservableMonitorComponent implements OnInit {
     this.observable.complete();
   }
 
-  repeatOften() {
-    const width = this.timeline.nativeElement.style.width.slice(0, -2) || '0';
-    this.timeline.nativeElement.style.width = parseInt(width) + 1 + 'px';
-    this.globalID = requestAnimationFrame(this.repeatOften.bind(this));
+  repeatOften(targetNode, level) {
+    const width = targetNode.style.width.slice(0, -2) || '0';
+    targetNode.style.width = parseInt(width) + 1 + 'px';
+    this.animationIds[level] = requestAnimationFrame(this.repeatOften.bind(this, targetNode, level));
   }
 
-  appendValue(value) {
+  repeat(targetNode) {
+    const width = targetNode.style.width.slice(0, -2) || '0';
+    targetNode.style.width = parseInt(width) + 1 + 'px';
+    this.globalID = requestAnimationFrame(this.repeat.bind(this, targetNode));
+  }
+
+  appendValue(targetNode, value) {
     if (value && value.type === MONITOR.VALUE) {
       const node = document.createElement('span');
       const textnode = document.createTextNode(value.value);
       node.appendChild(textnode);
-      node.style.left = document.getElementById('timeline').style.width;
+      node.style.left = targetNode.style.width;
       node.style.position = 'absolute';
-      this.timeline.nativeElement.appendChild(node);
+      targetNode.appendChild(node);
     } else if (value && value.type === MONITOR.SUBSCRIBE) {
       const node = document.createElement('span');
       const textnode = document.createTextNode('|');
@@ -76,23 +99,44 @@ export class ObservableMonitorComponent implements OnInit {
       node.style.left = document.getElementById('timeline').style.width;
       node.style.position = 'absolute';
       this.timeline.nativeElement.appendChild(node);
+    } else if (value && value.type === MONITOR.INNER) {
+      const node = document.createElement('div');
+      node.style.left = document.getElementById('timeline').style.width;
+      node.style.position = 'relative';
+      node.style.height = '20px';
+      node.style.width = '0px';
+      node.style.marginTop = '10px';
+      node.style.background = 'aqua';
+      this.animationIds[value.level] = requestAnimationFrame(this.repeatOften.bind(this, node, value.level));
+      this.container.nativeElement.appendChild(node);
+      this.subs[value.level] = value.value.subscribe(x => {
+        this.appendValue(node, {
+          value: x,
+          type: MONITOR.VALUE
+        });
+      });
     }
   }
 
   ngOnInit() {
      this.subscription = this.subject.subscribe((value) => {
-      this.appendValue(value);
+      this.appendValue(this.timeline.nativeElement, value);
     });
-
-    this.timeService.currentStatus.subscribe(status => {
+     this.timeService.currentStatus.subscribe(status => {
       if (status) {
-        this.globalID = requestAnimationFrame(this.repeatOften.bind(this));
+        this.globalID = requestAnimationFrame(this.repeat.bind(this, this.timeline.nativeElement));
         if (this.autoSub) {
           this.observe();
         }
       } else if (this.globalID) {
         cancelAnimationFrame(this.globalID);
+        for (const id of this.animationIds) {
+          cancelAnimationFrame(id);
+        }
         this.subscription.unsubscribe();
+        for (const sub of this.subs) {
+          sub.unsubscribe();
+        }
       }
     });
   }
