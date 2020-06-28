ace37539eaa0d3163a4762601205debb0be8e6db
qiuyunfen <yfqiu@thoughtworks.com>
2020-06-28 13:19:34 +0800

    multiple complete

--- src/app/multiple/multiple.component.ts
+++ src/app/multiple/multiple.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {interval, Observable} from 'rxjs';
+import {publish, publishReplay, refCount, share, shareReplay, take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-multiple',
@@ -7,7 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MultipleComponent implements OnInit {
 
-  constructor() { }
+  interval$: Observable<any>;
+  publish$: Observable<any>;
+  share$: Observable<any>;
+  shareReplay$: Observable<any>;
+
+  constructor() {
+    this.interval$ = interval(1000).pipe(take(5));
+    this.share$ = this.interval$.pipe(share());
+    this.publish$ = this.interval$.pipe(publish(), refCount());
+    this.shareReplay$ = this.interval$.pipe(shareReplay(1));
+  }
 
   ngOnInit() {
   }
